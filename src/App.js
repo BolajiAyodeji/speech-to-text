fd5d124bff6d5682f9f1412ba2303c151081a3c2
bolajiayodeji <sirbeejay1@gmail.com>
2019-07-27 16:11:24 +0100

    setup IBM watson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone'
+
+class App extends Component {
+  onListenClick() {
+    fetch('/api/speech-to-text/token')
+  .then(function(response) {
+      return response.text();
+  }).then(function (token) {
+
+    var stream = recognizeMic({
+        token: token, // use `access_token` as the parameter name if using an RC service
+        objectMode: true, // send objects instead of text
+        extractResults: true, // convert {results: [{alternatives:[...]}], result_index: 0} to {alternatives: [...], index: 0}
+        format: false // optional - performs basic formatting on the results such as capitals an periods
+    });
+
+    stream.on('data', function(data) {
+      console.log(data);
+    });
+
+    stream.on('error', function(err) {
+        console.log(err);
+    });
+
+    document.querySelector('#stop').onclick = stream.stop.bind(stream);
+
+  }).catch(function(error) {
+      console.log(error);
+  });
+ }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={this.onListenClick}>Listen to Mic</button>
+        </header>
+      </div>
+    );
+  }
+};
 
 export default App;
